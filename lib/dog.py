6e90c2a830c142ba1faae6f004129638048db60d
Job koech <jobkoech610@gmail.com>
2023-05-18 02:22:23 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,23 @@
 
 class Dog:
     # Class body goes here
-
+    
     #Instance method definition
-    pass
+    def bark(self):
+        print("Woof!")
+
+    def  sit(self):
+         print("The dog is sitting.")    
+
+    #class instance
+fido = Dog() 
+fido.bark()    
+
+snoopy = Dog()
+snoopy.bark()
+
+roxy = Dog()
+roxy.sit()
+
+    
+    
--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,15 @@ class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
+job = Person()
+job.talk()
+
+mike = Person()
+mike.walk()
+    
